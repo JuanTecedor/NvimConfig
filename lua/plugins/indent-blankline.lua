5fb58aa4e90e4fbe9520a49b4fb2ab2b53646cc5
Juan <43722131+JuanTecedor@users.noreply.github.com>
2024-01-30 19:30:31 +0100

    fix style

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,8 +1,8 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {},
-    setup = function()
-        require("ibl").setup()
-    end,
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	opts = {},
+	setup = function()
+		require("ibl").setup()
+	end,
 }
